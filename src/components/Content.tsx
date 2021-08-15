464c62dfc57513354a417f81a3e20ea509f2768a
Alcir Junior <junior@cajucomunica.com.br>
2021-08-15 20:01:00 -0300

    refactor: separate components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,50 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from './MovieCard';
+
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+type ContentProps = {
+  selectedGenre: {
+    id: number;
+    title: string;
+  };
+}
+
+export function Content({ selectedGenre: { id, title } }: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${id}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [id]);
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
